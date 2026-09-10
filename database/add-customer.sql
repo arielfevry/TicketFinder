dbe08ffdf8d6f6a5e5d78aa634a8177a19f965c0
lucas <moscosoleon2006@gmail.com>
2026-09-10 18:01:25 +0000

    finalized tables with data

---
+++ database/add-customer.sql
@@ -0,0 +1,4 @@
+INSERT INTO `Customer`(`FirstName`, `LastName`, `Username`, `Password`, `DateCreated`, `DateUpdated`) VALUES
+('Lucas', 'Moscoso', 'Ender', 'Customer1', '9/10/26', '9/10/26'),
+('Yo', 'Gurt', 'GurtMode', 'Mode-gurt', '9/9/26', '9/12/26'),
+('Rivers', 'Cuomo', 'Buddy', 'Holly', '9/8/26', '9/10/26');
