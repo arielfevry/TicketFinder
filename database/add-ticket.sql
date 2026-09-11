1f0501a038165fe842e125b6cff9ca420655b60a
lucas <moscosoleon2006@gmail.com>
2026-09-11 19:21:10 +0000

    updated tables

--- database/add-ticket.sql
+++ database/add-ticket.sql
@@ -1,5 +1,5 @@
-INSERT INTO `Ticket`(`MovieName`, `TheaterName`, `ShowTime`, `LocationAddress`, `CustomerID`, `DateCreated`, `DateUpdated`) VALUES
-('Atlantic Rim', 'AMC', '2:30pm', 'Easy Street', 1, '9/10/26', '9/10/26'),
-('Good Burger', 'Regal', '10:00am', 'Sunset Blvd', 1, '9/10/26', '9/10/26'),
-('Real Steel', 'Cinemark', '12:15pm', 'York Ave', 2, '9/9/26', '9/9/26'),
-('Atlantic Rim', 'AMC', '1:30', 'Park Ave', 3, '9/8/26', '9/8/26');
+INSERT INTO `Ticket`(`MovieName`, `TheaterName`, `ShowTime`, `LocationAddress`, `CustomerID`,`Age`, `DateCreated`, `DateUpdated`) VALUES
+('Atlantic Rim', 'AMC', '2:30pm', 'Easy Street', 1, 0, '9/10/26', '9/10/26'),
+('Good Burger', 'Regal', '10:00am', 'Sunset Blvd', 1, 0,  '9/10/26', '9/10/26'),
+('Real Steel', 'Cinemark', '12:15pm', 'York Ave', 2, 1, '9/9/26', '9/9/26'),
+('Atlantic Rim', 'AMC', '1:30', 'Park Ave', 3, 2, '9/8/26', '9/8/26');
--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,12 +1,30 @@
-CREATE TABLE `TicketDB`.`Customer`
+CREATE TABLE `TicketDB`.`User`
 (
     `ID` INT NOT NULL AUTO_INCREMENT,
     `FirstName` VARCHAR(50) NOT NULL DEFAULT '',
     `LastName` VARCHAR(50) NOT NULL DEFAULT '',
-    `Username` VARCHAR(50) NOT NULL DEFAULT '',
+    `Email` VARCHAR(50) NOT NULL DEFAULT '',
     `Password` VARCHAR(50) NOT NULL DEFAULT '',
     `DateCreated` VARCHAR(50) NOT NULL DEFAULT '',
     `DateUpdated` VARCHAR(50) NOT NULL DEFAULT '',
+    PRIMARY KEY (`ID`)
+)ENGINE = innoDB;
+
+CREATE TABLE `TicketDB`.`Admin`
+(
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `AdminID` INT NOT NULL DEFAULT '0',
+    PRIMARY KEY (`ID`),
+    Foreign Key (`AdminID`) REFERENCES User(`ID`)
+)ENGINE = innoDB;
+
+CREATE TABLE `TicketDB`.`Movie`
+(
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `Title` VARCHAR(50) NOT NULL DEFAULT '',
+    `Genre` VARCHAR(50) NOT NULL DEFAULT '',
+    `ReleaseDate` VARCHAR(50) NOT NULL DEFAULT '',
+    `ImageUrl` VARCHAR(100) NOT NULL DEFAULT '',
     PRIMARY KEY (`ID`)
 )ENGINE = innoDB;
 
@@ -14,12 +32,12 @@ CREATE TABLE `TicketDB`.`Ticket`
 (
     `ID` INT NOT NULL AUTO_INCREMENT,
     `MovieName` VARCHAR(50) NOT NULL DEFAULT '',
-    `TheaterName` VARCHAR(50) NOT NULL DEFAULT '',
     `ShowTime` VARCHAR(50) NOT NULL DEFAULT '',
     `LocationAddress` VARCHAR(50) NOT NULL DEFAULT '',
     `CustomerID` INT NOT NULL DEFAULT '0',
+    `Age` INT NOT NULL DEFAULT '0', --0 for adult, 1 for child, 2 for senior 
     `DateCreated` VARCHAR(50) NOT NULL DEFAULT '',
     `DateUpdated` VARCHAR(50) NOT NULL DEFAULT '',
     PRIMARY KEY (`ID`),
-    FOREIGN KEY (`CustomerID`) REFERENCES Customer(`ID`)
+    FOREIGN KEY (`CustomerID`) REFERENCES User(`ID`)
 )ENGINE = innoDB;
