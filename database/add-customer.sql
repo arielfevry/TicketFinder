7e35469b95e5c096c126c47a81ec973ac1b96b8a
lucas <moscosoleon2006@gmail.com>
2026-09-11 19:21:10 +0000

    updated tables

--- database/add-customer.sql
+++ database/add-customer.sql
@@ -1,4 +1,7 @@
-INSERT INTO `Customer`(`FirstName`, `LastName`, `Username`, `Password`, `DateCreated`, `DateUpdated`) VALUES
+INSERT INTO `User`(`FirstName`, `LastName`, `Username`, `Password`, `DateCreated`, `DateUpdated`) VALUES
 ('Lucas', 'Moscoso', 'Ender', 'Customer1', '9/10/26', '9/10/26'),
 ('Yo', 'Gurt', 'GurtMode', 'Mode-gurt', '9/9/26', '9/12/26'),
 ('Rivers', 'Cuomo', 'Buddy', 'Holly', '9/8/26', '9/10/26');
+
+INSERT INTO `Admin`(`AdminID`) VALUES
+(1);
