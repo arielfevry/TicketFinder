19cbbd7d296a58db2d70a994ab751ac3b71e6e8c
lucas <moscosoleon2006@gmail.com>
2026-09-15 18:31:07 +0000

    Finalized DB for now + created a admin user

--- database/add-customer.sql
+++ database/add-customer.sql
@@ -1,7 +1,7 @@
-INSERT INTO `User`(`FirstName`, `LastName`, `Username`, `Password`, `DateCreated`, `DateUpdated`) VALUES
-('Lucas', 'Moscoso', 'Ender', 'Customer1', '9/10/26', '9/10/26'),
-('Yo', 'Gurt', 'GurtMode', 'Mode-gurt', '9/9/26', '9/12/26'),
-('Rivers', 'Cuomo', 'Buddy', 'Holly', '9/8/26', '9/10/26');
+INSERT INTO `User`(`FirstName`, `LastName`, `Email`, `Password`, `DateCreated`, `DateUpdated`) VALUES
+('Lucas', 'Moscoso', 'example@gmail.com', 'Customer1', '9/10/26', '9/10/26'),
+('Yo', 'Gurt', 'Gurt@gmail.com', 'Mode-gurt', '9/9/26', '9/12/26'),
+('Rivers', 'Cuomo', 'Buddy@gmail.com', 'Holly', '9/8/26', '9/10/26');
 
 INSERT INTO `Admin`(`AdminID`) VALUES
 (1);
