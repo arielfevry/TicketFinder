de7e64a4b8391dd331aa035075d57a3610667561
Lucas Moscoso-Leon <56801095+Endermnce@users.noreply.github.com>
2026-09-18 14:58:51 -0400

    EDITED TABLES

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -30,16 +30,30 @@ CREATE TABLE `TicketDB`.`Movie`
     PRIMARY KEY (`ID`)
 )ENGINE = innoDB;
 
+CREATE TABLE `TicketDB`.`ShowTimes`
+(
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `MovieID` INT NOT NULL DEFAULT 0,
+    `Seats` INT NOT NULL DEFAULT 24,
+    `Datetime` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01',
+    `Location` VARCHAR(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`ID`),
+    Foreign Key (`MovieID`) REFERENCES Movie(`ID`)
+)ENGINE = innoDB;
+
 CREATE TABLE `TicketDB`.`Ticket`
 (
     `ID` INT NOT NULL AUTO_INCREMENT,
+    `ShowTimeID` INT NOT NULL DEFAULT 0,
     `MovieName` VARCHAR(50) NOT NULL DEFAULT '',
     `ShowTime` VARCHAR(50) NOT NULL DEFAULT '',
     `LocationAddress` VARCHAR(50) NOT NULL DEFAULT '',
+    `SeatNumber` VARCHAR(50) NOT NULL DEFAULT '',
     `CustomerID` INT NOT NULL DEFAULT '0',
     `Age` INT NOT NULL DEFAULT '0', 
     `DateCreated` VARCHAR(50) NOT NULL DEFAULT '',
     `DateUpdated` VARCHAR(50) NOT NULL DEFAULT '',
     PRIMARY KEY (`ID`),
-    FOREIGN KEY (`CustomerID`) REFERENCES User(`ID`)
+    FOREIGN KEY (`CustomerID`) REFERENCES User(`ID`),
+    Foreign Key (`ShowTimeID`) REFERENCES ShowTimes(`ID`)
 )ENGINE = innoDB;
