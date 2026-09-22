29afedfe49899f22d1d040fd602d62efe514167c
Ariel Fevry <arielfevry@gmail.com>
2026-09-22 12:05:04 -0400

    feat: configure the apache and php web broswer routes to api files

---
+++ index.php
@@ -0,0 +1,59 @@
+\<?php
+
+require_once __DIR__ . '/api/config/helpers.php';
+
+setCORSHeaders();
+header("Content-Type: application/json; charset=UTF-8");
+
+// 1. get URI request from broswer
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// 2. convert requests to simple paths
+$path = str_replace('/TicketFinder', '', $request);
+$path = trim($path, '/');
+
+// 3. explode path to get resource IDs
+$fields = explode('/', $path);
+$resource = $fields[0] ?? ''; 
+
+$GLOBALS['RESOURCE_ID'] = null; // default resource id to null
+
+if(isset($fields[1]) && is_numeric($fields[1])){  //if resource requested by ID
+	$GLOBALS['RESOURCE_ID'] = (int)$fields[1]; // save globally
+}
+
+// 4. route resource request to appropriate api files
+switch($resource) {
+	// public routes
+        case 'login':
+		require __DIR__ . '/api/auth/login.php';
+		break;
+
+	case 'register':
+		require __DIR__ . '/api/auth/register.php';
+		break;
+
+	// authetication required routs 
+	case 'tickets':
+		$GLOBALS['USER_ID'] = requireAuth();
+
+		require __DIR__ . '/api/tickets.php';
+		break;
+
+	case 'movies':
+		$GLOBALS['USER_ID'] = requireAuth();
+
+		require __DIR__ . '/api/movies.php';
+		break;
+
+	case 'admins':
+		$GLOBALS['USER_ID'] = requireAuth();
+		requireAdmin($GLOBALS['USER_ID']);
+
+		require __DIR__ . '/api/admins.php';
+		break;
+
+	default: //invalid path 
+		respond(404, ["error" => "Endpoint not found"]);
+		break;
+}
