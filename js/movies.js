805635ade5df743442978a12b26f516b0f0c9269
samuel olarte <samu.ola33@gmail.com>
2026-09-24 12:24:13 -0400

    Added some of the front end for movies, but needed to push to access on my home pc

---
+++ js/movies.js
@@ -0,0 +1,55 @@
+/* async function loadMovies() {
+    try {
+        const response = await fetch("movies");
+
+        console.log("Status:", response.status);
+
+        const data = await response.text();
+
+        console.log("Backend response:", data);
+    }
+    catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+loadMovies(); */
+
+const movies = [
+    {
+        ID: 1,
+        Title: "The Fortnite Movie",
+        Genre: "Action",
+        ReleaseDate: "2026-09-01",
+        ImageUrl: "cat_img.jpg"
+    },
+    {
+        ID: 2,
+        Title: "Movie Two",
+        Genre: "Comedy",
+        ReleaseDate: "2026-09-10",
+        ImageUrl: "cat_img.jpg"
+    }
+];
+const movieList = document.getElementById("movie-list");
+
+movies.forEach(function(movie) {
+
+    const movieCard = document.createElement("div");
+
+    movieCard.innerHTML = `
+        <img src="${movie.ImageUrl}" alt="${movie.Title}" width="200">
+
+        <h3>${movie.Title}</h3>
+
+        <p>${movie.Genre}</p>
+
+        <p>${movie.ReleaseDate}</p>
+
+        <a href="movie.html?id=${movie.ID}">
+            View Showtimes
+        </a>
+    `;
+
+    movieList.appendChild(movieCard);
+});
